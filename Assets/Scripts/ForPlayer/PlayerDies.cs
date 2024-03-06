9202d6221ae562c96c837b698857e1f3d6c9aec8
Isabel <dojofxseito@gmail.com>
2024-03-06 11:58:58 -0800

    edits to ship and playerdies
    player dies edit

--- Assets/Scripts/ForPlayer/PlayerDies.cs
+++ Assets/Scripts/ForPlayer/PlayerDies.cs
@@ -8,6 +8,25 @@ public class PlayerDies : MonoBehaviour
     public void KillPlayer()
     {
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-        gameObject.SetActive(false);
+        //disable image and move script so player essentially "dies"
+        //cant use set active or else player dies script wont work 
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<PlayerShip>().enabled = false;
+
+        //a lil delay before respawn
+
+        //if(lives => 0) { }
+        StartCoroutine(RespawnDelay());
     }
+    IEnumerator RespawnDelay()
+    {
+        yield return new WaitForSeconds(3f);
+
+        // This line will be executed after 3 seconds passed
+        GetComponent<Renderer>().enabled = true;
+        GetComponent<PlayerShip>().enabled = true;
+        //reset pos
+        gameObject.transform.position = new Vector3 (0, 0, 0);
+    }
+
 }
