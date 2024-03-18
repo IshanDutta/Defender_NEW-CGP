055f2cb639e3e8c1c46b64af18bf969e4533d09b
Isabel <dojofxseito@gmail.com>
2024-03-18 08:22:28 -0700

    CameraFollow
    to better fit hyperspace
    reimplemented some changes
    R for restart

--- Assets/Scripts/CameraFollow - Copy.cs
+++ Assets/Scripts/CameraFollow - Copy.cs
@@ -9,37 +9,44 @@ public class CameraFollow : MonoBehaviour
     public float followSpeed = 2f;
     public Transform target;
     public float xOffset = 1f;
+    public bool doLerp = true;
+    public float hyperspaceLerp = 0.1f; 
 
     // Update is called once per frame
     void Update()
     {
+        if(doLerp == true)
+        {
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            //restarts level 
-            string currentSceneName = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(currentSceneName);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow)  && xOffset < 0)
-        {
-            xOffset = xOffset * -1;
-        }
+            if (Input.GetKeyDown(KeyCode.RightArrow) && xOffset < 0)
+            {
+                xOffset = xOffset * -1;
+            }
 
-        else if (Input.GetKeyDown(KeyCode.D) && xOffset < 0)
-        {
-            xOffset = xOffset * -1;
-        }
+            else if (Input.GetKeyDown(KeyCode.D) && xOffset < 0)
+            {
+                xOffset = xOffset * -1;
+            }
 
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && xOffset > 0)
-        {
-            xOffset = xOffset * -1;
-        }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && xOffset > 0)
+            {
+                xOffset = xOffset * -1;
+            }
 
-        else if (Input.GetKeyDown(KeyCode.A) && xOffset > 0)
-        {
-            xOffset = xOffset * -1;
+            else if (Input.GetKeyDown(KeyCode.A) && xOffset > 0)
+            {
+                xOffset = xOffset * -1;
+            }
+            Vector3 newpos = new Vector3(target.position.x + xOffset, 1f, -20f);
+            transform.position = Vector3.Slerp(transform.position, new Vector3(newpos.x, 0, newpos.z), followSpeed * Time.deltaTime);
         }
-        Vector3 newpos = new Vector3(target.position.x + xOffset, 1f, -20f);
-        transform.position = Vector3.Slerp(transform.position, new Vector3(newpos.x, 0, newpos.z), followSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            //go instantly there, no transition 
+            doLerp = false;
+            Vector3 afterhyperspace = new Vector3(target.position.x, target.position.y, target.position.z);
+            //transform.position = Vector3.Slerp(transform.position, afterhyperspace, hyperspaceLerp * Time.deltaTime);
+            doLerp = true;
+        }
     }
 }
